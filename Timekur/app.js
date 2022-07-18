30e3b8f6f331d6eacd191161540ea0a110a7dcc3
pewnior <maciek.pewniak@gmail.com>
2022-07-18 19:42:10 +0200

    Update

--- Timekur/app.js
+++ Timekur/app.js
@@ -3,26 +3,17 @@ function oblicz(){
     var szyb=document.getElementById("szybkosc").value;
     var grand=document.getElementById("grand").checked;
     var mlode=document.getElementById("mlode").checked;
-    if (!dys.length || dys==0) {
-        document.getElementById("wynik").innerHTML="Błąd";
-        document.getElementById("dystans").style.borderWidth="3px";
-        document.getElementById("dystans").style.borderColor="red";
-    }
-    else if (grand==true && mlode==false && (szyb==300 || szyb==325) ){
+    if (grand==true && mlode==false && (szyb==300 || szyb==325) ){
         document.getElementById("wynik").innerHTML="W konkursach Grand Prix szybkość może wynosić od 375 do 400 m/min lub 350 m/min w konkursach halowych i konkursach rozgrywanych na arenie otwartej o wymiarach 65 m x 85 m lub mniejszych. "
-        document.getElementById("szybkosc").style.borderWidth="3px";
-        document.getElementById("szybkosc").style.borderColor="red";
+        document.getElementById("szybkosc").classList.add('error');
     }
-    else if(mlode==true && szyb != 325){
+    else if(mlode==true && szyb!=325){
         document.getElementById("wynik").innerHTML="W konkurach dla młodych koni szybkość powinna wynosić 325 m/min";
-        document.getElementById("szybkosc").style.borderWidth="3px";
-        document.getElementById("szybkosc").style.borderColor="red";
+        document.getElementById("szybkosc").classList.add('error');
     }
     else{
-        document.getElementById("dystans").style.borderWidth="1.5px";
-        document.getElementById("szybkosc").style.borderWidth="1.5px";
-        document.getElementById("dystans").style.borderColor="black";
-        document.getElementById("szybkosc").style.borderColor="black";
+        document.getElementById("szybkosc").classList.remove('error');
+        document.getElementById("szybkosc").classList.add('good');
         document.getElementById("wynik").innerHTML="Norma czasu powinna wynosić "+Math.round(dys/szyb*60);
     }
 }
--- Timekur/app_en.js
+++ Timekur/app_en.js
@@ -3,26 +3,17 @@ function oblicz(){
     var szyb=document.getElementById("szybkosc").value;
     var grand=document.getElementById("grand").checked;
     var mlode=document.getElementById("mlode").checked;
-    if (!dys.length || dys==0) {
-        document.getElementById("wynik").innerHTML="Error";
-        document.getElementById("dystans").style.borderWidth="3px";
-        document.getElementById("dystans").style.borderColor="red";
-    }
-    else if (grand==true && mlode==false && (szyb==300 || szyb==325) ){
+    if (grand==true && mlode==false && (szyb==300 || szyb==325) ){
         document.getElementById("wynik").innerHTML="In Grand Prix pace must be from 375 to 400 m/min or 350 m/min in indoor competitions and competitions held in an open arena measuring 65 m x 85 m or less."
-        document.getElementById("szybkosc").style.borderWidth="3px";
-        document.getElementById("szybkosc").style.borderColor="red";
+        document.getElementById("szybkosc").classList.add('error');
     }
     else if(mlode==true && szyb != 325){
         document.getElementById("wynik").innerHTML="In competition for young horses pace must be 325 m/min";
-        document.getElementById("szybkosc").style.borderWidth="3px";
-        document.getElementById("szybkosc").style.borderColor="red";
+        document.getElementById("szybkosc").classList.add('error');
     }
     else{
         document.getElementById("wynik").innerHTML="Time allowed must be  "+Math.round(dys/szyb*60);
-        document.getElementById("dystans").style.borderWidth="1.5px";
-        document.getElementById("szybkosc").style.borderWidth="1.5px";
-        document.getElementById("dystans").style.borderColor="black";
-        document.getElementById("szybkosc").style.borderColor="black";
+        document.getElementById("szybkosc").classList.remove('error');
+        document.getElementById("szybkosc").classList.add('good');
     }
 }
