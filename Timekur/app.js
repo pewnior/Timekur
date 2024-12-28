44f468263a4d3ab207a933e424c21671b5e82aeb
pewnior <maciek.pewniak@gmail.com>
2024-12-28 12:48:46 +0100

    Update app.js

--- Timekur/app.js
+++ Timekur/app.js
@@ -2,7 +2,7 @@ function oblicz(){
     var dys=document.getElementById("dystans").value;
     var szyb=document.getElementById("szybkosc").value;
     var grand=document.getElementById("grand").checked;
-    var mlode=document.getElementById("mlode").checked;
+    var mlode=document.getElementById("mlode").checked; 
     if (grand==true && mlode==false && (szyb==300 || szyb==325) ){
         document.getElementById("wynik").innerHTML="W konkursach Grand Prix szybkość może wynosić od 375 do 400 m/min lub 350 m/min w konkursach halowych i konkursach rozgrywanych na arenie otwartej o wymiarach 65 m x 85 m lub mniejszych. "
         document.getElementById("szybkosc").classList.add('error');
@@ -16,4 +16,5 @@ function oblicz(){
         document.getElementById("szybkosc").classList.add('good');
         document.getElementById("wynik").innerHTML="Norma czasu powinna wynosić "+Math.round(dys/szyb*60);
     }
+    
 }
